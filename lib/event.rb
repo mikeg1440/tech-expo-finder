3ac5af4e2dfe463bd6659247a7a55be52d2b86b1
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-24 14:41:42 -0400

    Added: New Event class instance variables to store more location information

--- lib/event.rb
+++ lib/event.rb
@@ -1,17 +1,22 @@
 
 class Event
 
-  attr_accessor :name, :location, :date, :country, :city
+  attr_accessor :name, :location, :date, :country, :city, :state
 
   @@all = []
 
-  def initialize(name, date, country, city)
+  def initialize(name, date, country, city, state)
 
     @name = name
     @date = date
     @country = country
     @city = city
-    @location = [city, country]
+    @state = state
+    @location = {
+      city: city,
+      state: state,
+      country: country
+    }
 
     @@all << self
   end
