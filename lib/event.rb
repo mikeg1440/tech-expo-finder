0a0b31524747c1e46efc1bb2c0dd7f916dec2670
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-24 13:59:17 -0400

    Added: Methods to Event and Scraper classes (from the lib folder)

--- lib/event.rb
+++ lib/event.rb
@@ -1,5 +1,23 @@
 
 class Event
 
+  attr_accessor :name, :location, :date, :country, :city
+
+  @@all = []
+
+  def initialize(name, date, country, city)
+
+    @name = name
+    @date = date
+    @country = country
+    @city = city
+    @location = [city, country]
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,18 @@
-
+require 'open-uri'
+require 'nokogiri'
 
 class Scraper
+
+  attr_accessor
+
+  def initialize(url)
+
+    html = open(url)
+
+    Nokogiri::HTML(html)
+
+  end
+
 
 
 
