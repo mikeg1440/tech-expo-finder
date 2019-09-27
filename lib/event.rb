5a32a171525cbff5f024b0b037e0bebb9f81dc1e
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-27 04:20:04 -0400

    Modified: Event and Scraper class (in the lib folder) to adhere more to the single responsibility principle.

--- lib/event.rb
+++ lib/event.rb
@@ -25,4 +25,65 @@ class Event
     @@all
   end
 
+  def print_events(events)
+    events.each do |event|
+      print "Event: "
+      print "#{event.name}\n".green
+      print "\tEvent Date: "
+      print "#{event.date}\n".green
+      print "\tEvent Location: "
+      print "#{event.city}, #{event.country}\n\n".green
+    end
+  end
+
+  def get_events_by_state(state)
+    events = get_events_in_country("USA")
+
+    in_state = events.find_all {|event| event.state == state}
+
+    binding.pry
+
+    in_state
+  end
+
+  def get_events_by_country(country)
+    Event.all.find_all {|event| event.country == country}
+  end
+
+  def get_events_state(element)
+
+    url = element.css("td strong a").first['href']
+
+    binding.pry
+    puts "Getting Cities State From: #{url}"
+
+    html = open(url)
+
+    doc = Nokogiri::HTML(html)
+
+    # binding.pry
+
+    doc.css(".bld").text
+  end
+
+  def create_events(doc)
+
+    doc.css("tbody tr").each do |element|
+
+      next if element.text == "RankEventWhenWhereCategoryRating"
+
+      if element.text != ""
+        name = element.css(".box-link strong").text.strip
+        date = element.css("td strong")[1].text.strip
+        country = element.css("td a.block").text.strip
+        city = element.css("td small.text-muted").first.text.strip
+        state = get_event_state(element)
+
+        Event.new(name, date, country, city, state)
+        binding.pry
+      end
+
+    end
+
+  end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,19 +1,44 @@
 require 'open-uri'
 require 'nokogiri'
+require 'pry-moves'
 
 class Scraper
 
-  attr_accessor
+  attr_accessor :url, :doc
 
-  def initialize(url)
+  @@all = []
 
+  def initialize(url = "pages/top100.html")
+
+    @url = url
+
+    if url != "pages/top100.html"
+      open_from_url(url)
+    else
+      open_from_file(url)
+    end
+  end
+
+  def open_from_file(filepath = "pages/top100.html")
+    url = File.read(filepath)
+    @doc = Nokogiri::HTML(url)
+    save
+    @doc
+  end
+
+  def open_from_url(url)
     html = open(url)
-
-    Nokogiri::HTML(html)
-
+    @doc = Nokogiri::HTML(html)
+    save
+    @doc
   end
 
 
+  private
+
+  def save
+    @@all << self
+  end
 
 
 end
