991d5ff65d186b12ad53e126cb051ffa812e6c65
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-27 05:56:55 -0400

    Modified: lib/event_scraper_cli.rb to relfect changes\n Added: a menu and options for finding events

--- lib/event_scraper_cli.rb
+++ lib/event_scraper_cli.rb
@@ -1,15 +1,73 @@
+require 'colorize'
+
 
 class ScraperCLI
 
-  attr_accessor :name, :url
+  attr_accessor :url, :user
   attr_reader :scraper
 
   @@all = []
 
-  def initialize(name)
-    @name = name
-    @scraper = Scraper.new 
+  def initialize
+    @scraper = Scraper.new
+    @scraper.scrape_and_create_events(@scraper.doc)
+    @user = create_new_user
     save
+    show_menu
+  end
+
+  def show_menu
+
+    puts "Hello User!".green
+
+    loop do
+      puts "1. Show Events by Country".green
+      puts "2. Show Events by State".green
+      puts "3. Show Events by City".green
+      puts "4. Show Events By Date".green
+      puts "5. Get Event Info By Name".green
+      puts "0. Exit".red
+
+      print "Please Pick A Valid Menu Number: ".blue
+      menu_pick = gets.chomp
+
+      menu_pick == "0" ? break : handle_input(menu_pick)
+
+    end
+
+    binding.pry
+  end
+
+  def handle_input(menu_pick)
+    case menu_pick
+    when "1"
+      get_events_by_country
+    when "2"
+      get_events_by_state
+    when "3"
+      get_events_by_city
+    when "4"
+      get_events_by_date
+    when "5"
+      get_event_by_name
+    else
+      puts "Invalid Menu Choice\nPlease Pick From One of The Menu Options!".red
+    end
+  end
+
+  
+
+  def create_new_user
+    User.new("user_#{User.all.count+1}")
+  end
+
+  def get_user_zipcode
+    print "Please Enter Your Zip-Code: ".blue
+    gets.chomp
+  end
+
+  def self.all
+    @@all
   end
 
 
