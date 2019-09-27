530d71496d0a25c3e248701282d14c3fe4915e0b
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-27 05:52:50 -0400

    Modified: config/environment.rb to include the new User and Location class

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ Bundler.require
 require_relative '../lib/event_scraper_cli'
 require_relative '../lib/event'
 require_relative '../lib/scraper'
+require_relative '../lib/user'
+require_relative '../lib/location'
