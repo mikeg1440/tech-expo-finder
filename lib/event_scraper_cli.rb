cd98270fd06daae3cce14a730b9fbee59ca68d9e
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-29 00:21:29 -0400

    Feat: created the methods that find and list cities where events are being held and asks user to choose a city then lists events, asks for user to pick event then lists event info.

--- lib/event_scraper_cli.rb
+++ lib/event_scraper_cli.rb
@@ -22,10 +22,10 @@ class ScraperCLI
 
     loop do
       puts "1. Show Events by Country".green
-      puts "2. Show Events by State".green
-      puts "3. Show Events by City".green
-      puts "4. Show Events By Date".green
-      puts "5. Get Event Info By Name".green
+      puts "2. Show Events by City".green
+      puts "3. Show Events By Date".green
+      puts "4. Get Event Info By Name".green
+      puts "5. Show Events by Region".green
       puts "0. Exit".red
 
       print "Please Pick A Valid Menu Number: ".blue
@@ -43,16 +43,47 @@ class ScraperCLI
     when "1"
       find_events_by_country
     when "2"
-      get_events_by_state
+      find_events_by_city
     when "3"
-      get_events_by_city
-    when "4"
       get_events_by_date
-    when "5"
+    when "4"
       get_event_by_name
+    when "5"
+      get_events_by_region
     else
       puts "Invalid Menu Choice\nPlease Pick From One of The Menu Options!".red
     end
+  end
+
+  # displays list of states and country then asks user to pick a state
+  def find_events_by_city
+
+    sorted_by_cities = Location.all.uniq {|location| location.city}.sort_by { |l| l.city }
+
+    sorted_by_cities.each_with_index do |location, index|
+      puts "#{index+1}. #{location.city}, #{location.country}".green
+    end
+
+    puts "00. Exit".red
+
+    city_choice = nil
+
+    loop do
+      print "Choose a City to View Events: ".blue
+      city_choice = gets.chomp
+
+      break if city_choice.to_i.between?(1, sorted_by_cities.count) || city_choice == "00"
+    end
+
+    # city = sorted_by_cities[city_choice.to_i - 1].city
+    # country = sorted_by_cities[city_choice.to_i - 1].country
+
+    # get_events_by_location(city, country) unless country_choice == "00"
+
+    location = sorted_by_cities[city_choice.to_i - 1]
+
+    get_events_by_location(location) unless city_choice == "00"
+
   end
 
 
@@ -61,10 +92,10 @@ class ScraperCLI
 
     country_choice = nil
 
-    countries = Location.all.uniq {|location| location.country }.sort_by {|location| location.country}
-
-    countries = countries.sort_by {|location| location.country}
-
+    # countries = Location.all.uniq {|location| location.country }.sort_by {|location| location.country}
+    #
+    # countries = countries.sort_by {|location| location.country}
+    countries = Location.countries
     countries.each.with_index do |location, index|
       puts "#{index+1}. #{location.country}".green
 
@@ -73,10 +104,11 @@ class ScraperCLI
     puts "00. Exit".red
 
     loop do
+
       print "Choose a Country to View Events: ".blue
       country_choice = gets.chomp
 
-      break if country_choice.to_i.between?(1, Event.all.count) || country_choice == "00"
+      break if country_choice.to_i.between?(1, countries.count) || country_choice == "00"
     end
 
     country = countries[country_choice.to_i - 1].country
@@ -107,6 +139,30 @@ class ScraperCLI
     end
 
     events[event_choice.to_i - 1].print_event_information
+
+  end
+
+  def get_events_by_location(location)
+
+    events = Event.all.find_all {|event| event.location == location }.sort_by {|e| e.city}
+
+    events.each_with_index do |event, index|
+      puts "#{index+1}. #{event.name}".green
+    end
+
+    event_choice = nil
+
+    loop do
+
+      puts "00. Exit".red
+      print "Enter a Event's Number: ".blue
+      event_choice = gets.chomp
+
+      break if event_choice.to_i.between?(1, events.count) || event_choice == "00"
+    end
+
+    events[event_choice.to_i - 1].print_event_information
+
 
   end
 
