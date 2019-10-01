f332f09e1989f5e659d0f5d62aecf8b2e5aedce0
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-01 02:43:10 -0400

    Modified: code in lib/event.rb so that unused code is commented out, didn't delete for possible future use

--- lib/event.rb
+++ lib/event.rb
@@ -23,32 +23,32 @@ class Event
     Event.all.find_all {|event| event.location == location }
   end
 
-  def self.get_events_by_state(state = "USA")
-    # events = get_events_in_country("USA")
-
-    # in_state = events.find_all {|event| event.state == state}
-
-    # in_state
-    events.find_all {|event| event.state == state}.sort_by {|event| event.location.country}
-  end
+  # def self.get_events_by_state(state = "USA")
+  #   # events = get_events_in_country("USA")
+  #
+  #   # in_state = events.find_all {|event| event.state == state}
+  #
+  #   # in_state
+  #   get_events_in_country.find_all {|event| event.state == state}.sort_by {|event| event.location.country}
+  # end
 
   def self.get_events_by_country(country)
     self.all.find_all {|event| event.location.country == country}.sort_by {|event| event.location.country}
   end
 
-  def get_events_state(element)
-
-    url = element.css("td strong a").first['href']
-
-    binding.pry
-    puts "Getting Cities State From: #{url}"
-
-    html = open(url)
-
-    doc = Nokogiri::HTML(html)
-
-    doc.css(".bld").text
-  end
+  # def get_events_state(element)
+  #
+  #   url = element.css("td strong a").first['href']
+  #
+  #   binding.pry
+  #   puts "Getting Cities State From: #{url}"
+  #
+  #   html = open(url)
+  #
+  #   doc = Nokogiri::HTML(html)
+  #
+  #   doc.css(".bld").text
+  # end
 
   # prints out current instances information
   def print_event_information
