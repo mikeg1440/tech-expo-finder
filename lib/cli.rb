d5bff3e98c38e20bb2640d2fcb9c7d3743d62f18
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-02 20:04:41 -0400

    Refactor : [lib/cli.rb] - To DRY out code
      Made code that was being called in multiple methods into a method that 
    accepts the array of event objects and a question string to prompt the 
    user for a response, then returns the event object the user picked.

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,53 +10,72 @@ class EventScraperCli::CLI
     show_menu
   end
 
+  # displays menu options to user and prompts for a selection
   def show_menu
-    puts "Hello User!".green
+    puts "Hello User!".blue
 
     loop do
-      puts "1. Show Events by Country".green
-      puts "2. Show Events by City".green
-      puts "3. Show Events By Date".green
-      puts "4. Get Event Info By Name".green
-      # puts "5. Show Events by Region".green
+      puts "Please Pick a Option 1-4 or type in command within brackets".blue
+      puts "1. Show Events by Country - [find by country]".green
+      puts "2. Show Events by City - [find by city]".green
+      puts "3. Show Events By Date - [find by date]".green
+      puts "4. Get Event Info By Name - [find by name]".green
       puts "!. Reload File".magenta
       puts "0. Exit".red
 
-      print "Please Pick A Valid Menu Number: ".blue
-      menu_pick = gets.chomp
+      print "Please Pick A Valid Menu Number[1-4]: ".blue
+      menu_pick = gets.chomp.downcase
 
-      menu_pick == "0" ? break : handle_input(menu_pick)
+      if menu_pick == "0" || menu_pick == "exit"
+        break
+      else
+        event = handle_input(menu_pick)
+      end
       menu_pick = nil
+      display_info(event) if event
     end
 
   end
 
+  # handles user's menu choice and calls corresponding method
   def handle_input(menu_pick)
 
-    case menu_pick.downcase
-    when "0" || "exit"
+    case menu_pick
+    when "0", "exit"
       return
-    when "1" || "find by country"
-      find_events_by_country
-    when "2" || "find by city"
-      find_events_by_city
-    when "3" || "find by date"
-      find_events_by_date
-    when "4" || "find by name"
-      find_events_by_name
-    when "!" || "reload"
+    when "1", "find by country"
+      event = find_events_by_country
+    when "2", "find by city"
+      event = find_events_by_city
+    when "3", "find by date"
+      event = find_events_by_date
+    when "4", "find by name"
+      event = find_events_by_name
+    when "!", "reload"
       reload_file
     else
       puts "Invalid Menu Choice\nPlease Pick From One of The Menu Options!".red
     end
-    puts "PRINT EVENT INFO AND ASK OPEN IN BROWSER QUESTION".red
 
+    event
   end
 
+  # method accepts a scraper object as argument and calls scrape_and_create_events on the object to scrape data and create event and location objects
   def scrape_events(scraper)
     scraper.scrape_and_create_events
   end
 
+  # takes a event object argument and calls print_event_information on the object
+  def display_info(event)
+
+    event.print_event_information
+
+    open_in_browser(event)
+
+  end
+
+  # [*] this method is for getting user's choice of event after events are listed with coralating numbers [*]
+  # takes a question as a string and max option number then displays question and gets/validates user input
   def get_user_reponse(question, max_option)
     response = ""
     until response.to_i.between?(1, max_option) || response == "00"
@@ -94,6 +113,8 @@ class EventScraperCli::CLI
 
     countries = EventScraperCli::Location.countries
 
+
+    # [----] MAKE THIS NEXT SECTION OF CODE INTO A METHOD IF ITS USED AGIAN ANYWHERE ELSE IN THIS CLASS [----]
     countries.each.with_index do |location, index|
       puts "#{index+1}. #{location.country}".green
     end
@@ -108,9 +129,7 @@ class EventScraperCli::CLI
 
     country = countries[country_choice.to_i - 1].country
 
-    if country_choice == "00"
-      return
-    end
+    # [----] UP UNTIL THIS POINT [----]
 
     events = EventScraperCli::Event.get_events_by_country(country)
 
@@ -120,10 +139,12 @@ class EventScraperCli::CLI
 
     chosen_event ? get_details(chosen_event) : return
 
-    chosen_event.print_event_information
-
-    open_in_browser(chosen_event)
+    # chosen_event.print_event_information
+    #
+    # open_in_browser(chosen_event)
+    chosen_event
   end
+
 
   # asks user for a date then finds all events going on during that day
   def find_events_by_date
@@ -140,6 +161,8 @@ class EventScraperCli::CLI
 
     events = EventScraperCli::Event.all.find_all {|event| user_date.between?(event.start_date, event.end_date) }
 
+    binding.pry
+
     if events.empty?
       print "No Events Found on the date: ".red
       print "#{user_date.month}-#{user_date.day}-#{user_date.year}\n".green
@@ -152,8 +175,8 @@ class EventScraperCli::CLI
 
     get_details(chosen_event)
 
-    chosen_event.print_event_information
-
+    # chosen_event.print_event_information
+    chosen_event
   end
 
   # asks user for a search string and finds all events with any match to that string, then asks what event they want details of
@@ -172,10 +195,10 @@ class EventScraperCli::CLI
 
     # get_details(chosen_event)
 
-    chosen_event.print_event_information
-
-    open_in_browser(chosen_event)
-
+    # chosen_event.print_event_information
+    #
+    # open_in_browser(chosen_event)
+    chosen_event
   end
 
   # this method takes a array of event objects plus a question to prompt user then lists events with numbers, gets and returns user reponse to question
@@ -236,10 +259,11 @@ class EventScraperCli::CLI
 
     get_details(chosen_event)
 
-    chosen_event.print_event_information
+    # chosen_event.print_event_information
+    #
+    # open_in_browser(chosen_event)
 
-    open_in_browser(chosen_event)
-
+    chosen_event
   end
 
 
