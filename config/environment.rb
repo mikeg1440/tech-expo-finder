302b0d66d5b7247102cac0f0ec7ab660f15e4311
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-04 19:58:47 -0400

    Fix: [lib/*|bin/*|config/*] - Change file directory structure
    Made the project file structure reflect what is expected via the 
    project guidlines.

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,7 @@ require 'bundler'
 require 'open-uri'
 Bundler.require(:default, :developement)
 
-module EventScraperCli
-  require_relative '../lib/cli'
-  require_relative '../lib/event'
-  require_relative '../lib/scraper'
-  require_relative '../lib/location'
-end
+require_relative '../lib/cli'
+require_relative '../lib/event'
+require_relative '../lib/scraper'
+require_relative '../lib/location'
---
+++ lib/event_finder.rb
@@ -0,0 +1,4 @@
+module EventScraperCli
+end
+
+require_relative "../config/environment"
