71deef897e95e5cd5cab8c54880c2a2942820dc9
Mike Gaudreau <gaudreaum01@gmail.com>
2019-10-04 20:06:45 -0400

    Fix: [config/*] - File paths to reflect changes made to files

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'bundler'
 require 'open-uri'
 Bundler.require(:default, :developement)
 
-require_relative '../lib/cli'
-require_relative '../lib/event'
-require_relative '../lib/scraper'
-require_relative '../lib/location'
+require_relative '../lib/event_finder/cli'
+require_relative '../lib/event_finder/event'
+require_relative '../lib/event_finder/scraper'
+require_relative '../lib/event_finder/location'
