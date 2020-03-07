98f58f854545d890d93d1e4f1956657c3feb3107
Mike Gaudreau <gaudreaum01@gmail.com>
2020-03-07 16:02:09 -0500

    Add additional instance variables to event class

--- lib/event_finder/event.rb
+++ lib/event_finder/event.rb
@@ -1,7 +1,10 @@
 
 class EventScraperCli::Event
 
-  attr_accessor :name, :location, :start_date, :end_date, :date_string, :url, :hours, :days, :description, :users, :exhibitors, :photos, :photo_url, :price,:rating, :visitors, :expected_exhibitors, :category, :time_data
+  attr_accessor :name, :location, :start_date, :end_date, :date_string, :url, :hours, :days,
+  :description, :users, :exhibitors, :photos, :photo_url, :price,:rating, :visitors, :expected_exhibitors,
+  :category, :time_data, :country, :city
+
   @@all = []
 
   def initialize( name:, start_date:, end_date:, date_string:, city:, country:, url: )
